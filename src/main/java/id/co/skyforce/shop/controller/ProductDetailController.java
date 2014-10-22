aecac1bdcabe828ea5cf8a238e89b21c858e9ba2
Septiandhy <sandy.septiandhy@windowslive.com>
2014-10-22 17:29:07 +0700

    tambah form di product detail

---
+++ src/main/java/id/co/skyforce/shop/controller/ProductDetailController.java
@@ -0,0 +1,27 @@
+package id.co.skyforce.shop.controller;
+
+import id.co.skyforce.shop.model.Product;
+import id.co.skyforce.shop.service.ProductDetailService;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ProductDetailController {
+	private Product product;
+	
+	
+	
+	public ProductDetailController() {
+		ProductDetailService detailService = new ProductDetailService();
+		product = detailService.getDetailProduct();
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+	
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+}
---
+++ src/main/java/id/co/skyforce/shop/service/CustomerService.java
@@ -0,0 +1,18 @@
+package id.co.skyforce.shop.service;
+
+import java.util.Date;
+
+import id.co.skyforce.shop.model.Address;
+import id.co.skyforce.shop.model.Customer;
+import id.co.skyforce.shop.util.HibernateUtil;
+
+import javax.faces.bean.ManagedBean;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@ManagedBean
+public class CustomerService {
+	
+
+}
