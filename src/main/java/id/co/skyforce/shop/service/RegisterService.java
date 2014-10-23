e2cea0ad904e5d4198cf1d8e7318fcc25adb0112
irwansyahazniel <irwansyahazniel@gmail.com>
2014-10-23 10:25:02 +0700

    Controller dan Service Register

---
+++ src/main/java/id/co/skyforce/shop/service/RegisterService.java
@@ -0,0 +1,22 @@
+package id.co.skyforce.shop.service;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import id.co.skyforce.shop.model.Customer;
+import id.co.skyforce.shop.util.HibernateUtil;
+
+public class RegisterService {
+	
+	public void register(Customer customer) {
+		
+		Session session = HibernateUtil.openSession();
+		Transaction trx = session.beginTransaction();
+		
+		session.save(customer);
+		trx.commit();
+		session.close();
+		
+	}
+	
+}
