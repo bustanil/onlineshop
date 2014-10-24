1daf11583714619fde1e4db2a46947bef691158c
irwansyahazniel <irwansyahazniel@gmail.com>
2014-10-24 13:52:16 +0700

    product manajemen for admin

--- src/main/java/id/co/skyforce/shop/controller/ProductController.java
+++ src/main/java/id/co/skyforce/shop/controller/ProductController.java
@@ -1,11 +1,14 @@
 package id.co.skyforce.shop.controller;
 
+import id.co.skyforce.shop.model.Category;
+import id.co.skyforce.shop.model.Product;
+import id.co.skyforce.shop.model.Supplier;
+import id.co.skyforce.shop.service.ProductService;
 import id.co.skyforce.shop.util.HibernateUtil;
-import id.co.skyforce.shop.model.*;
-
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
@@ -17,10 +20,11 @@ import org.hibernate.Transaction;
 /**
  * 
  * @author Sandy Septiandhy
+ * @author Irwansyah Hazniel
  *
  */
 
-@ManagedBean(name="pc")
+@ManagedBean
 public class ProductController {
 
 	private Long productId;
@@ -29,60 +33,112 @@ public class ProductController {
 	private Integer stock;
 	private String description;
 	private Long categoryId;
-
+	private Long supplierId;
+	
+	private List<Category> categories;
+	private List<Supplier> suppliers;
+	
 	public ProductController() {
-		String idProduct = FacesContext.getCurrentInstance()
-				.getExternalContext().getRequestParameterMap().get("id");
-
-		if(idProduct != null){
-			productId = Long.valueOf(idProduct);
+		
+		String passProductId = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap().get("product_id");
+//		String passCategoryId = FacesContext.getCurrentInstance()
+//				.getExternalContext().getRequestParameterMap().get("category_id");
+//		String passSupplierId = FacesContext.getCurrentInstance()
+//				.getExternalContext().getRequestParameterMap().get("supplier_id");
+		
+		if (passProductId != null) {
+			
+			this.productId = Long.valueOf(productId);
+			
 			Session session = HibernateUtil.openSession();
 			Transaction trx = session.beginTransaction();
-			Product p = (Product) session.get(Product.class, productId);
-			trx.commit();session.close();
-
-			name = p.getName();
-			price = p.getPrice();
-			stock = p.getStock();
-			description = p.getDescription();
-
-			categoryId = p.getCategory().getId();
+			
+			Product product = (Product) session.get(Product.class, this.productId);
+			
+			// temporary change
+//			System.out.println("id = " + product.getId() + ", name = " + product.getName() 
+//					+ ", description = " + product.getDescription() + ", price = " + product.getPrice() + 
+//					", stock = " + product.getStock());
+			
+			this.name = product.getName();
+			this.price = product.getPrice();
+			this.stock = product.getStock();
+			this.description = product.getDescription();
+			this.categoryId = product.getCategory().getId();
+			this.supplierId = product.getSupplier().getId();
+			
+			categories = session.createQuery("FROM Category").list();
+			suppliers = session.createQuery("FROM Supplier").list();
+			
+			trx.commit();
+			session.close();
+			
 		}
+		
 	}
-
-	public void save(){
+	
+	public String save() {
+		
+		Product product = new Product();
+		
 		Session session = HibernateUtil.openSession();
 		Transaction trx = session.beginTransaction();
-		Category cat = (Category) session.get(Category.class, categoryId);
-
-		Product p = new Product();
-		p.setId(productId);
-		p.setName(name);
-		p.setPrice(price);
-		p.setStock(stock);
-		p.setDescription(description);
-		p.setCategory(cat);
-		session.saveOrUpdate(p);
+		
+		Category category = (Category) session.get(Category.class, categoryId);
+		Supplier supplier = (Supplier) session.get(Supplier.class, supplierId);
+		
 		trx.commit();
 		session.close();
 		
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		// jika not null, otomatis update
+		product.setId(productId);
+		
+		product.setName(name);
+		product.setPrice(price);
+		product.setStock(stock);
+		product.setDescription(description);
+		
+		product.setCategory(category);
+		product.setSupplier(supplier);
+		
+		// jika not null, otomatis update
+//		category.setId(this.categoryId);
+		
+		// jika not null, otomatis update
+//		supplier.setId(this.supplierId);
+		
+		product.setCategory(category);
+		product.setSupplier(supplier);
+		
+		ProductService proService = new ProductService();
+		proService.saveService(product);
+		
+
+		ExternalContext externalContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
 		try {
-			externalContext.redirect("list_product.xhtml");
+			externalContext.redirect("listproduct");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public void delete(){
 		
+		return "listproduct";
+		
+//		return "link kemana?";
+		
+	}
+
+	public void delete() {
+
 		Session session = HibernateUtil.openSession();
 		Transaction trx = session.beginTransaction();
 		Product p = (Product) session.get(Product.class, productId);
 		session.delete(p);
 		trx.commit();
 		session.close();
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		ExternalContext externalContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
 		try {
 			externalContext.redirect("list_product.xhtml");
 		} catch (IOException e) {
@@ -93,40 +149,57 @@ public class ProductController {
 	public Long getProductId() {
 		return productId;
 	}
+
 	public void setProductId(Long productId) {
 		this.productId = productId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public BigDecimal getPrice() {
 		return price;
 	}
+
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
+
 	public Integer getStock() {
 		return stock;
 	}
+
 	public void setStock(Integer stock) {
 		this.stock = stock;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public Long getCategoryId() {
-		return categoryId;
+	public List<Category> getCategories() {
+		return categories;
 	}
 
-	public void setCategoryId(Long categoryId) {
-		this.categoryId = categoryId;
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
+	public List<Supplier> getSuppliers() {
+		return suppliers;
+	}
+
+	public void setSuppliers(List<Supplier> suppliers) {
+		this.suppliers = suppliers;
+	}
+	
 }
--- src/main/java/id/co/skyforce/shop/service/ProductService.java
+++ src/main/java/id/co/skyforce/shop/service/ProductService.java
@@ -1,15 +1,33 @@
 package id.co.skyforce.shop.service;
 
+import id.co.skyforce.shop.model.Category;
 import id.co.skyforce.shop.model.Product;
 import id.co.skyforce.shop.util.HibernateUtil;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class ProductService {
+/**
+ * 
+ * @author Irwansyah Hazniel
+ *
+ */
 
+public class ProductService {
+	
+	
+	public void saveService(Product product) {
+		Session session = HibernateUtil.openSession();
+		Transaction trx = session.beginTransaction();
+		
+		session.saveOrUpdate(product);
+		
+		trx.commit();
+		session.close();
+	}
 	
 }
