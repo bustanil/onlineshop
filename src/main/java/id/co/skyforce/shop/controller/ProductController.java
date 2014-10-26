9e94a22c5765568bcfc352d7e80d49a8b9e4f3f2
irwansyahazniel <irwansyahazniel@gmail.com>
2014-10-26 13:36:49 +0700

    ProductController searchByName, berhasil, hore! :D

--- src/main/java/id/co/skyforce/shop/controller/ProductController.java
+++ src/main/java/id/co/skyforce/shop/controller/ProductController.java
@@ -1,16 +1,17 @@
 package id.co.skyforce.shop.controller;
 
-
 import id.co.skyforce.shop.model.Category;
 import id.co.skyforce.shop.model.Product;
 import id.co.skyforce.shop.model.Supplier;
 import id.co.skyforce.shop.service.ProductListService;
 import id.co.skyforce.shop.service.ProductService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 /**
@@ -33,24 +34,25 @@ public class ProductController {
 	private Long categoryId;
 	private Long supplierId;
 	private String keyword;
-	
+
 	private List<Product> products;
 	private List<Category> categories;
 	private List<Supplier> suppliers;
-	
+
 	public ProductController() {
-		
-		String passProductId = FacesContext.getCurrentInstance().getExternalContext()
-				.getRequestParameterMap().get("product_id");
-		
+
+		String passProductId = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap()
+				.get("product_id");
+
 		ProductService proService = new ProductService();
-		
+
 		if (passProductId != null) {
-			
+
 			productId = Long.valueOf(passProductId);
 
 			Product product = proService.getProduct(productId);
-			
+
 			name = product.getName();
 			price = product.getPrice();
 			stock = product.getStock();
@@ -59,61 +61,72 @@ public class ProductController {
 			supplierId = product.getSupplier().getId();
 
 		}
-		
+
 		categories = proService.getAllCategories();
 		suppliers = proService.getAllSuppliers();
-		
+
 	}
-	
+
 	public String searchByName() {
-		
+
 		ProductService proService = new ProductService();
-		
+
 		this.products = proService.searchByNameService(this.keyword);
+
+		ProductListController proListController = new ProductListController();
+		proListController.setPrd(null);
 		
 		return "list";
+
 	}
 
 	public String save() {
-		
+
 		ProductService proService = new ProductService();
-		
+
 		Category category = proService.getCategory(this.categoryId);
 		Supplier supplier = proService.getSupplier(this.supplierId);
-		Product product = new Product(this.name, this.price, this.stock, this.description);
-		
+		Product product = new Product(this.name, this.price, this.stock,
+				this.description);
+
 		// jika productId not null, update
 		product.setId(this.productId);
-		
+
 		product.setCategory(category);
 		product.setSupplier(supplier);
-		
+
 		proService.saveService(product);
-		
+
 		return "listproduct";
-		
+
 	}
-	
-	public String delete() {
-		
-		String passProductId = FacesContext.getCurrentInstance().getExternalContext()
-				.getRequestParameterMap().get("idDelete");
+
+	public void delete() {
+
+		String passProductId = FacesContext.getCurrentInstance()
+				.getExternalContext().getRequestParameterMap().get("idDelete");
 		this.productId = Long.valueOf(passProductId);
-		
+
 		ProductService proService = new ProductService();
 		proService.deleteService(this.productId);
-		
+
 		// update object list, setelah dihapus
 		ProductListService proListService = new ProductListService();
-		List <Product> products = proListService.getAllProduct();
-		
+		List<Product> products = proListService.getAllProduct();
+
 		ProductListController proListController = new ProductListController();
 		proListController.setPrd(products);
-		
-		return "listproduct"; 
-		
+
+		ExternalContext externalContext = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		try {
+			externalContext.redirect("listproduct.xhtml");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	public List<Supplier> getSuppliers() {
 		return suppliers;
 	}
@@ -129,7 +142,6 @@ public class ProductController {
 	public void setCategories(List<Category> categories) {
 		this.categories = categories;
 	}
-
 
 	public Long getSupplierId() {
 		return supplierId;
@@ -218,5 +230,5 @@ public class ProductController {
 	public void setProducts(List<Product> products) {
 		this.products = products;
 	}
-	
+
 }
