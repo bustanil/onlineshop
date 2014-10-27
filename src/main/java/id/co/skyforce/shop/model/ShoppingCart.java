7e199eb83b81f837c3be9d1bfcdb8c6a0174d388
irwansyahazniel <irwansyahazniel@gmail.com>
2014-10-27 09:26:18 +0700

    tambah table baru ShoppingCart

--- src/main/java/id/co/skyforce/shop/model/ShoppingCart.java
+++ src/main/java/id/co/skyforce/shop/model/ShoppingCart.java
@@ -1,20 +1,58 @@
 package id.co.skyforce.shop.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * 
  * @author Irwansyah Hazniel
  *
  */
 
+@Entity
+@Table(name="shopping_cart")
 public class ShoppingCart {
-	private Integer countItem = 0;
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id", nullable=false)
+	private Long id;
+	
+	@Column(name="total_item", nullable=false)
+	private Integer totalItem;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
 
-	public Integer getCountItem() {
-		return countItem;
+	public Long getId() {
+		return id;
 	}
 
-	public void setCountItem(Integer countItem) {
-		this.countItem = countItem;
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getTotalItem() {
+		return totalItem;
+	}
+
+	public void setTotalItem(Integer totalItem) {
+		this.totalItem = totalItem;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 	
 }
